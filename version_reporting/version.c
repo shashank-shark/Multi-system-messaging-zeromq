576f4f5ed26dbfa49bd5845706bacf15aa9a052b
shashank-shark <shashankreddy231@gmail.com>
2019-08-08 18:19:17 +0530

    ventilator program
    ventilator program.

--- version_reporting/version.c
+++ version_reporting/version.c
@@ -1,1 +1,10 @@
-#include <zmq.h>
+#include <stdio.h>
+#include "zhelpers.h"
+
+int main (int argc, char **argv)
+{
+	int major, minor, patch;
+	zmq_version (&major, &minor, &patch);
+	printf ("Current ZMQ versions is : %d %d %d\n", major, minor, patch);
+	return 0;
+}
