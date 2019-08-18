6f44d529d1e986930cfc9ead3d4899c4582ece07
shashank-shark <shashankreddy231@gmail.com>
2019-08-18 20:37:05 +0530

    Update load_balancer.c
    queue dequeue of workers

--- load_balancer_2/load_balancer.c
+++ load_balancer_2/load_balancer.c
@@ -6,6 +6,8 @@
 #define NBR_CLIENTS 30
 #define NBR_WORKERS 3
 
+#define DEQUEUE(q) memmove (&(q)[0], &(q)[1], sizeof (q) - sizeof(q[0]))
+
 /* client_work */
 static void *
 client_work (void *args)
@@ -95,6 +97,86 @@ int main (int argc, char **argv)
 		pthread_t worker;
 		pthread_create (&worker, NULL, worker_work, (void *)(intptr_t) worker_work);
 	}
+
+	// actual load balance here
+	int available_workers = 0;
+	char *worker_queue[10];
+
+	while (1)
+	{
+		// poll the network
+		zmq_pollitem_t items[] = {
+			{ backend, 0, ZMQ_POLLIN, 0},
+			{ frontend, 0, ZMQ_POLLIN, 0}
+		};
+
+
+		int rc = zmq_poll (items, available_workers ? 2 : 1, -1);
+		if (rc == -1)
+			break;
+
+		if (items[0].revents & ZMQ_POLLIN)
+		{
+			// the first frame contains the worker ID
+			char *worker_id = s_recv (backend);
+			assert (available_workers < NBR_WORKERS);
+			worker_queue [available_workers ++] = worker_id;
+
+			// second frame is empty
+			char *empty = s_recv (backend);
+			assert (empty[0] == 0);
+			free (empty);
+
+			// third frame is READY or else client reply identity
+			char *client_id = s_recv (backend);
+
+			// if client reply, send the rest back to the frontend
+			if (strcmp (client_id, "READY") != 0)
+			{
+				empty = s_recv (backend);
+				assert (empty[0] == 0);
+				free (empty);
+
+				char *reply = s_recv (backend);
+				s_sendmore (frontend, client_id);
+				s_sendmore (frontend, "");
+				s_send (frontend, reply);
+
+				free (reply);
+
+				if (-- client_nbr == 0)
+					break;
+
+			}
+
+			free (client_id);
+		}
+
+		if (items[0].revents & ZMQ_POLLIN)
+		{
+			char *client_id = s_recv (frontend);
+			char *empty = s_recv (frontend);
+			assert (empty[0] == 0);
+
+			char *request = s_recv (frontend);
+			s_sendmore (backend, worker_queue[0]);
+			s_sendmore (backend, "");
+			s_sendmore (backend, client_id);
+			s_sendmore (backend, "");
+			s_send (backend, request);
+
+			free (client_id);
+			free (request);
+
+			free (worker_queue[0]);
+			DEQUEUE (worker_queue);
+			available_workers --;
+		}
+	}
+
+	zmq_close (frontend);
+	zmq_close (backend);
+	zmq_ctx_destroy (context);
 
 	return 0;
 }
