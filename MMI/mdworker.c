21f35afcf4cd7a442a88c8d2d30e08c2e689e077
shashank-shark <shashankreddy231@gmail.com>
2019-09-24 18:54:34 +0530

    modified worker for zmsg

--- MMI/mdworker.c
+++ MMI/mdworker.c
@@ -3,6 +3,8 @@
 
 //  Lets us build this source without creating a library
 #include "mdwrkapi.c"
+#include "zhelpers.h"
+
 
 int main (int argc, char *argv [])
 {
@@ -10,13 +12,20 @@ int main (int argc, char *argv [])
     mdwrk_t *session = mdwrk_new (
         "tcp://localhost:5555", "echo", verbose);
 
+    void *context = zmq_ctx_new();
+    void *publisher = zmq_socket (context, ZMQ_PUB);
+    int rc = zmq_bind (publisher, "tcp://*:5556");
+    assert (rc == 0);
+
     zmsg_t *reply = NULL;
     while (true) {
         zmsg_t *request = mdwrk_recv (session, &reply);
         if (request == NULL)
             break;              //  Worker was interrupted
         reply = request;        //  Echo is complex... :-)
+        s_send (publisher, (char *)reply);
     }
+    zmq_close (publisher);
     mdwrk_destroy (&session);
     return 0;
 }
